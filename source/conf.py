ff2c1c2f5aa2dff841ae9db8f0c30c6017e6f452
kasia-ta <82645464+kasia-ta@users.noreply.github.com>
2025-07-05 14:55:32 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Bezpieczenstwo'
+project = 'Sprawozdanie'
 copyright = '2025, Kasia Tarasek'
 author = 'Kasia Tarasek'
 release = '1'
